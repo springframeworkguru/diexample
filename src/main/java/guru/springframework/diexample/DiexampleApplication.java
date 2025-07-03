047b13570fdf3c9e37d546dcfc157511fd7bed2f
John Thompson <john@springframework.guru>
2025-07-03 06:05:05 -0400

    Init add

---
+++ src/main/java/guru/springframework/diexample/DiexampleApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.diexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/diexample/DiexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.diexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
